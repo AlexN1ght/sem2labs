af30b569fd4f9a7013cc4e263831dfdfaf92e0d9
AlexN1ght <alexiscom@icloud.com>
2019-04-10 16:31:19 +0000

    26 menu for stack

--- 25-26/main.c
+++ 25-26/main.c
@@ -4,15 +4,37 @@
 
 int main(void)
 {
-	int a;
-	Stack *A = stack_create();
-	while(scanf("%d", &a) == 1) {
-		stack_push(A, a);
+	char c;
+	int st;
+	int val;
+	Stack *A[10];
+	while (1){
+	scanf("%c %d", &c, &st);
+	switch (c) {
+		case 'c':
+			A[st] = stack_create();
+			break;
+		case 'd':
+			stack_delete(&A[st]);
+			break;
+		case 'i':
+			scanf("%d", &val);
+			stack_push(A[st], val);
+			break;
+		case 'o':
+			stack_pop(A[st]);
+			break;
+		case 's':
+			sort(A[st]);
+			break;
+		case 'p':
+			stack_print(A[st]);
+			break;
+		default:
+			printf("Wrong syntax\n");
+			break;
 	}
-	puts("-------------------------");
-	stack_print(A);
-	puts("-------------------------");
-	sort(A);
-	stack_print(A);
+	getchar();
+	}
 	return 0;	
 }
